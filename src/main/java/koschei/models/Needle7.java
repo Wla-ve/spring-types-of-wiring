4f4a574f35d76d6d2fa2b62caf553e656292a4da
Paladin <ghostofdeath2023@gmail.com>
2021-09-11 17:35:18 +0300

    Done

---
+++ src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,18 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Needle7 {
+
+    private final Death8 death8;
+
+    public Needle7(Death8 death8) {
+        this.death8 = death8;
+    }
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле :( " + "";
+    }
+}
